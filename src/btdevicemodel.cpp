51798c417545b7180d7afeb96a01a10ee0d28812
Dan Leinir Turthra Jensen <admin@leinir.dk>
2018-12-07 21:45:00 +0000

    A simple model to show what bluetooth devices are available

---
+++ src/btdevicemodel.cpp
@@ -0,0 +1,101 @@
+/*
+ *   Copyright 2018 Dan Leinir Turthra Jensen <admin@leinir.dk>
+ *
+ *   This program is free software; you can redistribute it and/or modify
+ *   it under the terms of the GNU Library General Public License as
+ *   published by the Free Software Foundation; either version 3, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU Library General Public License for more details
+ *
+ *   You should have received a copy of the GNU Library General Public License
+ *   along with this program; if not, see <https://www.gnu.org/licenses/>
+ */
+
+#include "btdevicemodel.h"
+
+class BTDeviceModel::Private
+{
+public:
+    Private() {}
+    ~Private() {}
+
+    QList<BTDeviceModel::Device*> devices;
+};
+
+BTDeviceModel::BTDeviceModel(QObject* parent)
+    : QAbstractListModel(parent)
+    , d(new Private)
+{
+}
+
+BTDeviceModel::~BTDeviceModel()
+{
+    delete d;
+}
+
+QHash< int, QByteArray > BTDeviceModel::roleNames() const
+{
+    QHash<int, QByteArray> roles;
+    roles[Name] = "name";
+    roles[DeviceID] = "deviceID";
+    return roles;
+}
+
+QVariant BTDeviceModel::data(const QModelIndex& index, int role) const
+{
+    QVariant value;
+    if(index.isValid() && index.row() > -1 && index.row() < d->devices.count()) {
+        Device* device = d->devices.at(index.row());
+        switch(role) {
+            case Name:
+                value = device->name;
+                break;
+            case DeviceID:
+                value = device->deviceID;
+                break;
+            default:
+                break;
+        }
+    };
+    return value;
+}
+
+int BTDeviceModel::rowCount(const QModelIndex& parent) const
+{
+    if(parent.isValid()) {
+        return 0;
+    }
+    return d->devices.count();
+}
+
+void BTDeviceModel::addDevice(Device* device)
+{
+    beginInsertRows(QModelIndex(), 0, 0);
+    d->devices.insert(0, device);
+    endInsertRows();
+}
+
+void BTDeviceModel::removeDevice(Device* device)
+{
+    int idx = d->devices.indexOf(device);
+    if (idx > -1) {
+        beginRemoveRows(QModelIndex(), idx, idx);
+        d->devices.removeAt(idx);
+        endRemoveRows();
+    }
+    delete device;
+}
+
+const BTDeviceModel::Device* BTDeviceModel::getDevice(const QString& deviceID) const
+{
+    foreach(const BTDeviceModel::Device* device, d->devices) {
+        if(device->deviceID == deviceID) {
+            return device;
+        }
+    }
+    return nullptr;
+}
---
+++ src/btdevicemodel.h
@@ -0,0 +1,69 @@
+/*
+ *   Copyright 2018 Dan Leinir Turthra Jensen <admin@leinir.dk>
+ *
+ *   This program is free software; you can redistribute it and/or modify
+ *   it under the terms of the GNU Library General Public License as
+ *   published by the Free Software Foundation; either version 3, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU Library General Public License for more details
+ *
+ *   You should have received a copy of the GNU Library General Public License
+ *   along with this program; if not, see <https://www.gnu.org/licenses/>
+ */
+
+#ifndef BTDEVICEMODEL_H
+#define BTDEVICEMODEL_H
+
+#include <QAbstractListModel>
+#include <QBluetoothDeviceInfo>
+
+class BTDeviceModel : public QAbstractListModel
+{
+    Q_OBJECT
+public:
+    explicit BTDeviceModel(QObject* parent = 0);
+    virtual ~BTDeviceModel();
+
+    enum Roles {
+        Name = Qt::UserRole + 1,
+        DeviceID
+    };
+
+    struct Device {
+    public:
+        QString name;
+        QString deviceID;
+        QBluetoothDeviceInfo deviceInfo;
+    };
+
+    QHash< int, QByteArray > roleNames() const override;
+    QVariant data(const QModelIndex& index, int role = Qt::DisplayRole) const override;
+    int rowCount(const QModelIndex& parent = QModelIndex()) const override;
+
+    /**
+     * Add a new device to the model.
+     * The new device is added at the start of the unsorted model
+     * The model takes ownership of the device, and deletion should not
+     * be done manually.
+     * @param device The new device to show in the model
+     */
+    void addDevice(Device* device);
+    /**
+     * Remove a device from the model.
+     * The entry will be deleted by this function, and you should not attempt to
+     * use the instance afterwards.
+     * If the device is not maintained by this model, it will still be deleted!
+     */
+    void removeDevice(Device* device);
+
+    const BTDeviceModel::Device* getDevice(const QString& deviceID) const;
+private:
+    class Private;
+    Private* d;
+};
+
+#endif//BTDEVICEMODEL_H
