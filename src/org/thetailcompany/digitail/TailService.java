61bbf50ac5a9f3c0d0d8c14306ced2d87d54d0ac
Dan Leinir Turthra Jensen <admin@leinir.dk>
2019-01-27 21:49:56 +0000

    Add basic java service code

---
+++ src/org/thetailcompany/digitail/TailService.java
@@ -0,0 +1,25 @@
+// java file goes in android/src/org/thetailcompany/digitail/TailService.java
+package org.thetailcompany.digitail;
+
+import android.content.Context;
+import android.content.Intent;
+import org.qtproject.qt5.android.bindings.QtService;
+
+public class TailService extends QtService
+{
+    public static void startTailService(Context ctx) {
+        ctx.startService(new Intent(ctx, TailService.class));
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        super.onStartCommand(intent, flags, startId);
+        return Service.START_STICKY;
+    }
+
+    @Override
+    public void onCreate() {
+        // Check if notification should be shown and do so if needed
+
+    }
+}
