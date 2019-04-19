0d615288011f7ed45ff5bf0775961ad10adb5ea5
Gilmanov Ildar <dev@ddwarf.org>
2019-04-19 22:12:33 +0700

    Add stubs for PhoneEventsHandler class

---
+++ src/PhoneEventsHandler.h
@@ -0,0 +1,33 @@
+/*
+ *   Copyright 2019 Ildar Gilmanov <gil.ildar@gmail.com>
+ *
+ *   This program is free software; you can redistribute it and/or modify
+ *   it under the terms of the GNU Library General Public License as
+ *   published by the Free Software Foundation; either version 3, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU Library General Public License for more details
+ *
+ *   You should have received a copy of the GNU Library General Public License
+ *   along with this program; if not, see <https://www.gnu.org/licenses/>
+ */
+
+#ifndef PHONE_EVENTS_HANDLER_H
+#define PHONE_EVENTS_HANDLER_H
+
+#include <QObject>
+
+/**
+ * PhoneEventsHandler is used to handle and manage phone events, like calls, sms and etc.
+ */
+class PhoneEventsHandler
+{
+public:
+
+private:
+};
+
+#endif // PHONE_EVENTS_HANDLER_H
