e632e72005136266ec25af982c3d54c6a3bc80be
Dan Leinir Turthra Jensen <admin@leinir.dk>
2019-10-14 10:04:11 +0100

    Add some work to handle the new NotificationChannel requirement

--- data/src/org/thetailcompany/digitail/TailService.java
+++ data/src/org/thetailcompany/digitail/TailService.java
@@ -3,11 +3,14 @@ package org.thetailcompany.digitail;
 
 import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
 import android.os.PowerManager;
 import android.os.PowerManager.WakeLock;
 import android.app.ActivityManager;
 import android.app.ActivityManager.RunningServiceInfo;
 import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.NotificationChannel;
 import android.app.PendingIntent;
 import android.app.Service;
 import org.qtproject.qt5.android.bindings.QtService;
@@ -16,19 +19,43 @@ public class TailService extends QtService
 {
     private static WakeLock mWakeLock;
     private static final int ONGOING_NOTIFICATION_ID = 1;
+    private static final String NOTIFICATION_CHANNEL_ID = "org.thetailcompany.digitail";
+
+    private synchronized String createChannel() {
+        NotificationManager mNotificationManager = (NotificationManager) this.getSystemService(Context.NOTIFICATION_SERVICE);
+
+        String channelName = "TailService";
+        int importance = NotificationManager.IMPORTANCE_LOW;
+
+        NotificationChannel mChannel = new NotificationChannel(NOTIFICATION_CHANNEL_ID, channelName, importance);
+
+        if (mNotificationManager != null) {
+            mNotificationManager.createNotificationChannel(mChannel);
+        } else {
+            stopSelf();
+        }
+        return NOTIFICATION_CHANNEL_ID;
+    }
+
     public void acquireWakeLock() {
         PowerManager powerManager = (PowerManager) getSystemService(POWER_SERVICE);
         mWakeLock = powerManager.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK,
             "DIGITAiL::TailWakeLockTag");
         mWakeLock.acquire();
+
         // Start foregrounding the service and put up a notification saying we're alive
         Intent notificationIntent = new Intent(this, TailActivity.class);
         notificationIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
         PendingIntent pendingIntent =
                 PendingIntent.getActivity(this, 0, notificationIntent, PendingIntent.FLAG_UPDATE_CURRENT);
 
-        Notification notification =
-                new Notification.Builder(this)
+        String channel;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O)
+            channel = createChannel();
+        else {
+            channel = "";
+        }
+        Notification notification = new Notification.Builder(this, channel)
                 .setContentTitle(getText(R.string.notification_title))
                 .setContentText(getText(R.string.notification_message))
                 .setSmallIcon(R.mipmap.ic_launcher)
