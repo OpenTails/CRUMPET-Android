ac1a9cfe2089f0db90bc333ee457247ab9a0dd43
Dan Leinir Turthra Jensen <admin@leinir.dk>
2020-05-15 11:12:05 +0100

    Clean up the documentation for the command persistence class

--- src/CommandPersistence.h
+++ src/CommandPersistence.h
@@ -53,6 +53,27 @@ public:
     virtual ~CommandPersistence();
 
     /**
+     * Set the the title, description, and commands list based on the json contained
+     * within the string.
+     *
+     * @note, unless the string is invalid json, this will reset
+     * those three properties, even if the document is empty.
+     *
+     * @param json A string which should contain a fully formed json document
+     * @return Whether or not the deserialisation was successful
+     * @see error()
+     * @see read()
+     */
+    bool deserialize(const QString& json);
+    /**
+     * Get the json serialised version of what is currently stored in this class.
+     * This is also what will be stored in the file when you write it.
+     * @return A json document in text format
+     * @see write()
+     */
+    QString serialized() const;
+
+    /**
      * Request that data is read from a previously set location.
      *
      * If the operation is successfully started, it will clear the
@@ -60,59 +81,34 @@ public:
      * successfully completed, these will be repopulated
      * with the data read from the file at the set location.
      *
-     * To see whether or not the operation completes successfully,
-     * subscribe to the finished signal. Any errors will be emitted
-     * through the error signal.
-     *
-     * You cannot start an operation (read or write) while another is
-     * already ongoing.
-     *
-     * @return Whether or not the operation was successfully started
+     * @return Whether or not the operation was successfully completed
      * @see setLocation(QUrl)
      * @see error(QString)
-     * @see finished(bool)
      */
     bool read();
     /**
      * Request that the commands, title, and description previously
      * set are written to the also previously set location.
      *
-     * To see whether or not the operation completes successfully,
-     * subscribe to the finished signal. Any errors will be emitted
-     * through the error signal.
-     *
-     * You cannot start an operation (read or write) while another is
-     * already ongoing.
-     *
-     * @return Whether or not the operation was successfully started
+     * @return Whether or not the operation was successfully completed
      * @see setLocation(QUrl)
      * @see error(QString)
-     * @see finished(bool)
      */
     bool write();
+
     /**
-     * Emitted when an error occurs during the process. It does not
-     * specify a severity level, only that an error occurred. If it
-     * was a critical error, it can be deduced from the finished signal
-     * being fired with a successfulness of false.
+     * Emitted when an error occurs during any process. It does not
+     * specify a severity level, only that an error occurred.
      * @param message A human-readable message describing the error
      */
     Q_SIGNAL void error(QString message);
     /**
      * The error message most recently emitted by error(QString).
      * Useful in case you wish to make a more serious point of showing
-     * the error after a failed completion (in the slot connected to
-     * finished(bool) for example).
+     * the error after a failed completion.
      * @return The most recently emitted error message
      */
     QString error() const;
-    /**
-     * Whether or not the most recently requested operation finished
-     * successfully.
-     * @param success True if the operation was successful, false if not
-     * @see error()
-     */
-    Q_SIGNAL void finished(bool success);
 
     QString filename() const;
     void setFilename(const QString &filename);
