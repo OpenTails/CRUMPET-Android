3c6832b57ce873c56b8e2a3d12c65658c6818b8f
Dan Leinir Turthra Jensen <admin@leinir.dk>
2021-05-13 16:16:56 +0100

    Move (and fix) the Messages.sh extraction script

---
+++ Messages.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+BASEDIR="./src/"                                            # root of translatable sources
+PROJECT="digitail"                                          # project name
+BUGADDR="https://github.com/MasterTailer/CRUMPET/issues"    # MSGID-Bugs
+WDIR=`pwd`                                                  # working dir
+
+echo "Extracting messages"
+cd ${BASEDIR}
+# see above on sorting
+find . -name '*.cpp' -o -name '*.h' -o -name '*.c' -o -name '*.qml' | sort > ${WDIR}/infiles.list
+cd ${WDIR}
+xgettext --from-code=UTF-8 -C -kde -ci18n -ki18n:1 -ki18nc:1c,2 -ki18np:1,2 -ki18ncp:1c,2,3 -ktr2i18n:1 \
+	-kI18N_NOOP:1 -kI18N_NOOP2:1c,2 -kaliasLocale -kki18n:1 -kki18nc:1c,2 -kki18np:1,2 -kki18ncp:1c,2,3 \
+	--msgid-bugs-address="${BUGADDR}" \
+	--files-from=infiles.list -D ${BASEDIR} -D ${WDIR} -o ${PROJECT}.pot || { echo "error while calling xgettext. aborting."; exit 1; }
+echo "Done extracting messages"
+
+
+echo "Merging translations"
+catalogs=`find . -name '*.po'`
+for cat in $catalogs; do
+  echo $cat
+  msgmerge -o $cat.new $cat ${PROJECT}.pot
+  mv $cat.new $cat
+done
+echo "Done merging translations"
+
+
+echo "Cleaning up"
+cd ${WDIR}
+rm infiles.list
+echo "Done"
--- src/Messages.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-xgettext --c++ --kde --from-code=UTF-8 -ci18n  -ki18n:1 -ki18nc:1c,2 -ki18np:1,2 -ki18ncp:1c,2,3 -kki18n:1 -kki18nc:1c,2 -kki18np:1,2 -kki18ncp:1c,2,3 -kI18N_NOOP:1 -kI18NC_NOOP:1c,2 --copyright-holder="The Tail Company" --msgid-bugs-address="https://github.com/MasterTailer/CRUMPET/issues" `find . -name \*.qml -or -name \*.cpp` -o po/digitail.pot
