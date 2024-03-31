56e6ca6a95b33dca2f1d8d57582ea973794d326e
Dan Leinir Turthra Jensen <admin@leinir.dk>
2024-03-31 10:25:43 +0100

    TailActivity: Remove unneeded hack from the olden days
    This seems to have been resolved upstream, but i'm just leaving it here
    for now, in case it turns out something like it's needed again, but...
    looks to have done the thing, and having it in place looks to block
    things for us, so that isn't great.

--- data/src/org/thetailcompany/digitail/TailActivity.java
+++ data/src/org/thetailcompany/digitail/TailActivity.java
@@ -11,11 +11,12 @@ import org.qtproject.qt.android.bindings.QtActivity;
 public class TailActivity extends org.qtproject.qt.android.bindings.QtActivity
 {
     private static final String TAG = "TailActivity";
-
-    @Override
-    protected void onPause()
-    {
-        super.onPause();
-        Log.v(TAG, "onPause - entered");
-    }
+    // No longer needed as of Qt6 i guess - and looks like it interferes with bringing the app back up from sleep as well, so probably not great anyway
+    // Leaving in place just for now, can be removed later if it does turn out to truly not to be a problem any longer
+    // @Override
+    // protected void onPause()
+    // {
+        // super.onPause();
+        // Log.v(TAG, "onPause - entered");
+    // }
 }
